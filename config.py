f0d224adaf469dbcba8538aa4644cbbbca5ccdce
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-06-15 07:42:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     # Telegram API
     API_ID = int(os.getenv("API_ID", "25887786"))
     API_HASH = os.getenv("API_HASH", "e4201277f5f2883f22c150167bd24479")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "8917961129:AAF3Dmr88w6nx5quMT9mc9XQ5NaJHe1_bSk")
 
     # Database
     MONGO_URL = os.getenv(
@@ -14,7 +14,7 @@ class Config:
     )
 
     # Bot Info
-    BOT_USERNAME = os.getenv("BOT_USERNAME", "@CricketLegacy2Bot")
+    BOT_USERNAME = os.getenv("BOT_USERNAME", "@testingpgcbot")
     SUPPORT_GROUP = os.getenv(
         "SUPPORT_GROUP",
         "https://t.me/+joF1bCfiMT9jMzVh"
@@ -27,14 +27,14 @@ class Config:
     # Owners
     OWNER_IDS = {
         int(x)
-        for x in os.getenv("OWNER_IDS", "8186068163").split()
+        for x in os.getenv("OWNER_IDS", "8933874700").split()
     }
 
     # GitHub
     GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
     UPSTREAM_REPO = os.getenv(
         "UPSTREAM_REPO",
-        "https://github.com/drexocoder-source/Cricsssketlegssacys"
+        "https://github.com/sparshshivhare2007-pixel/Zdasd"
     )
 
     # AI / NVIDIA
